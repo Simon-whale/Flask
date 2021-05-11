bf338075b3b01378f6c1f4cecccd9f152356421f
Simon-whale <83454878+Simon-whale@users.noreply.github.com>
2021-05-11 13:50:38 +0100

    message to remove flask_restplus as it is obsolete

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from flask import Flask
 from flask_restplus import Resource, Api
 
 # TODO: add functionality so that the API namespaces are automatically expanded
+# TODO: replace flask_restplus as this is now obsolete and not being maintained
 
 app = Flask(__name__)
 api = Api(app)
+
+
 experiment_api = api.namespace('Experiments', description='Experiment operations')
 second_namespace = api.namespace('Part Two', description='Second Part')
 
